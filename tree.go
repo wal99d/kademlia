dbce1b7b05f39123f6beabb23d3618f4fec7580b
WaL99D <WaL99D@gmail.com>
2017-10-05 00:08:56 +0300

    Implemented new approuch using what nictuku is using...

--- tree.go
+++ tree.go
@@ -1,96 +1,69 @@
 package kademlia
 
-import (
-	"errors"
-	"fmt"
-)
-
-type Node struct {
-	bucket Bucket
-	prefix int
-	peer   *Peer
-	left   *Node
-	right  *Node
+type hashTree struct {
+	zero, one *hashTree
+	peer      *Peer
 }
 
-func (n *Node) insert(p *Peer) error {
-	if n == nil {
-		return errors.New("Cannot insert bucket into empty Tree!!")
-	}
-	bucketIndex := p.id.Xor(n.peer.id).Prefixlen()
-	switch {
-	case bucketIndex >= n.prefix:
-		if n.right == nil {
-			n.right = &Node{
-				bucket: NewBucket(),
-				peer:   p,
-				prefix: bucketIndex,
-				left:   nil,
-				right:  nil,
-			}
-			n.bucket.InsertInBucket(p, bucketIndex)
-			return nil
-		}
-		n.bucket.InsertInBucket(p, bucketIndex)
-		return n.right.insert(p)
+func (h *hashTree) insert(p *Peer) error {
+	return h.add(p, 0)
+}
 
-	case bucketIndex < n.prefix:
-		if n.left == nil {
-			n.left = &Node{
-				bucket: NewBucket(),
-				peer:   p,
-				prefix: bucketIndex,
-				left:   nil,
-				right:  nil,
-			}
-			n.bucket.InsertInBucket(p, bucketIndex)
+func (h *hashTree) add(p *Peer, i int) error {
+
+	switch {
+	case i >= len(p.id)*8:
+		h.peer = p
+		return nil
+	case h.peer != nil:
+		if h.peer.id == p.id {
+			h.peer = p
 			return nil
 		}
-		n.bucket.InsertInBucket(p, bucketIndex)
-		return n.left.insert(p)
+		//start compressing hashTree
+		old := h.peer
+		h.peer = nil
+		h.compressHashTree(p, old, i)
+		return nil
+	case h.peer == nil:
+		postion := byte(i % 8)
+		pivotValue := byte(p.id[i/8])
+		if (pivotValue<<postion)&128 == 0 {
+			if h.zero == nil {
+				h.zero = &hashTree{peer: p}
+				return nil
+			}
+			h.zero.add(p, i+1)
+		} else {
+			if h.one == nil {
+				h.one = &hashTree{peer: p}
+				return nil
+			}
+			h.one.add(p, i+1)
+		}
 	}
 	return nil
 }
 
-type Tree struct {
-	Root *Node
-}
+func (h *hashTree) compressHashTree(p *Peer, old *Peer, i int) error {
+	postion := byte(i % 8)
+	pivotNewValue := byte(p.id[i/8])
+	msbNew := (pivotNewValue << postion) & 128
+	pivotOldValue := byte(old.id[i/8])
+	msbOld := (pivotOldValue << postion) & 128
 
-func NewTree() *Tree {
-	return &Tree{}
-}
-
-//Insert will add new peer to its bucket index by its prefix
-func (t *Tree) Insert(p *Peer) error {
-	if t.Root == nil {
-		t.Root = &Node{
-			bucket: NewBucket(),
-			peer:   p,
-			left:   nil,
-			right:  nil,
+	switch {
+	case msbNew == msbOld:
+		if msbNew == 0 {
+			h.zero = &hashTree{}
+			h.zero.compressHashTree(p, old, i+1)
+		} else {
+			h.one = &hashTree{}
+			h.one.compressHashTree(p, old, i+1)
 		}
-		return nil
+	case msbNew != msbOld:
+		h.add(p, i)
+		h.add(old, i)
 	}
-	return t.Root.insert(p)
-}
-
-//Traverse will traverse the tree from root till leaf
-func (t *Tree) Traverse(n *Node, f func(*Node)) {
-	if n == nil {
-		return
-	}
-	t.Traverse(n.left, f)
-	f(n)
-	t.Traverse(n.right, f)
-}
-
-//ShowTree will print an orgnized tree structure as string good for debugging purposes
-func (t Tree) ShowTree() {
-	t.Traverse(t.Root, func(n *Node) {
-		for _, v := range n.bucket.peerList {
-			for e := v.Front(); e != nil; e = e.Next() {
-				fmt.Print(n.peer.id, ": ", n.peer.address, ":", e.Value.(*Peer).id, "| \n")
-			}
-		}
-	})
+	return nil
 }
